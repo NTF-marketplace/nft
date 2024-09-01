1c4baa7afa29d5f5e34e182fd5a58550aebb4c11
min-96 <ajdcnd131@naver.com>
2024-09-01 19:31:33 +0900

    fix: add collectionLogo to NftMetadataResponse

--- src/main/kotlin/com/api/nft/domain/nft/repository/NftRepositorySupportImpl.kt
+++ src/main/kotlin/com/api/nft/domain/nft/repository/NftRepositorySupportImpl.kt
@@ -1,12 +1,9 @@
 package com.api.nft.domain.nft.repository
 
 import com.api.nft.controller.dto.NftMetadataResponse
-import com.api.nft.enums.ChainType
-import com.api.nft.enums.ContractType
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import java.math.BigDecimal
 
 class NftRepositorySupportImpl(
     private val r2dbcEntityTemplate: R2dbcEntityTemplate
@@ -22,9 +19,12 @@ class NftRepositorySupportImpl(
             n.collection_name AS collectionName,
             n.contract_type AS contractType,
             m.image AS image,
-            nl.price AS lastPrice
+            nl.price AS lastPrice,
+            c.logo AS collectionLogo
         FROM 
             nft n
+        LEFT JOIN 
+            collection c ON n.collection_name = c.name    
         LEFT JOIN 
             metadata m ON n.id = m.nft_id
         LEFT JOIN 
@@ -32,22 +32,10 @@ class NftRepositorySupportImpl(
         WHERE 
             n.id = :$1
     """
-
         return r2dbcEntityTemplate.databaseClient.sql(query)
             .bind(0, id)
-            .map { row, data ->
-                NftMetadataResponse(
-                    id = (row.get("id") as Number).toLong(),
-                    tokenId = row.get("tokenId", String::class.java)!!,
-                    tokenAddress = row.get("tokenAddress", String::class.java)!!,
-                    chainType = row.get("chainType", ChainType::class.java)!!,
-                    nftName = row.get("nftName", String::class.java)!!,
-                    collectionName = row.get("collectionName", String::class.java)!!,
-                    image = row.get("image", String::class.java) ?: "",
-                    contractType = row.get("contractType", ContractType::class.java)!!,
-                    lastPrice = row.get("lastPrice", BigDecimal::class.java)?.toDouble(),
-                )
-            }.first()
+            .map { row, _ -> NftMetadataResponse.fromRow(row) }
+            .first()
     }
 
     override fun findAllByNftJoinMetadata(ids: List<Long>): Flux<NftMetadataResponse> {
@@ -64,26 +52,17 @@ class NftRepositorySupportImpl(
         FROM 
             nft n
         LEFT JOIN 
+            collection c ON n.collection_name = c.name    
+        LEFT JOIN 
             metadata m ON n.id = m.nft_id
-        LEFT JOIN nft_listing nl ON nft.id = nl.nft_id    
+        LEFT JOIN 
+            nft_listing nl ON n.id = nl.nft_id    
         WHERE 
             n.id IN (:$1)
     """
         return r2dbcEntityTemplate.databaseClient.sql(query)
             .bind(0, ids)
-            .map { row, metadata ->
-                NftMetadataResponse(
-                    id = (row.get("id") as Number).toLong(),
-                    tokenId = row.get("tokenId", String::class.java)!!,
-                    tokenAddress = row.get("tokenAddress", String::class.java)!!,
-                    chainType = row.get("chainType", ChainType::class.java)!!,
-                    nftName = row.get("nftName", String::class.java)!!,
-                    collectionName = row.get("collectionName", String::class.java)!!,
-                    image = row.get("image", String::class.java) ?: "",
-                    contractType = row.get("contractType", ContractType::class.java)!!,
-                    lastPrice = row.get("lastPrice", BigDecimal::class.java)?.toDouble(),
-                )
-            }
+            .map { row, _ -> NftMetadataResponse.fromRow(row) }
             .all()
     }
 
